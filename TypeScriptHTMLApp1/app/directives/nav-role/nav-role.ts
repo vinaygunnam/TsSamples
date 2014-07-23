065391f4dd16e02ba9f20ec8895b91efc0c14241
Vinay Gunnam <vinaygunnam@live.com>
2014-07-22 22:48:51 -0500

    Sample app for nav menu

---
+++ TypeScriptHTMLApp1/app/directives/nav-role/nav-role.ts
@@ -0,0 +1,24 @@
+﻿module App {
+    class NavRoleDirective {
+        constructor() {
+            var directive: ng.IDirective = {
+                restrict: "A",
+                link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+                    // hide the sub menu
+                    var subMenu = element.find(".sub-menu").first();
+                    subMenu.hide();
+
+                    // attach click handler
+                    element.on("click", (evt: JQueryEventObject) => {
+                        subMenu.slideToggle();
+                        evt.stopPropagation();
+                    });
+                }
+            };
+
+            return directive;
+        }
+    }
+
+    container.directive("navRole", NavRoleDirective);
+} 
